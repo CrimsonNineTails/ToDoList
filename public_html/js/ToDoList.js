52fb818aeda4db11b1c62dcebe01e9782286b097
Yo1357 <anders.museth@gmail.com>
2016-04-20 12:37:00 -0700

    Bubble menu

---
+++ public_html/js/ToDoList.js
@@ -0,0 +1,40 @@
+$(function (){
+   var APPLICATION_ID = "8B1C2399-1443-6254-FFB8-C4C280680100",
+       SECRET_KEY = "F014B1D6-7772-4951-FFB2-160FAE296F00",
+       VERSION = "v1";
+       
+   Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+});
+
+            $(function() {
+                $('#nav > div').hover(
+                function () {
+                    var $this = $(this);
+                    $this.find('img').stop().animate({
+                        'width'     :'199px',
+                        'height'    :'199px',
+                        'top'       :'-25px',
+                        'left'      :'-25px',
+                        'opacity'   :'1.0'
+                    },500,'easeOutBack',function(){
+                        $(this).parent().find('ul').fadeIn(700);
+                    });
+
+                    $this.find('a:first,h2').addClass('active');
+                },
+                function () {
+                    var $this = $(this);
+                    $this.find('ul').fadeOut(500);
+                    $this.find('img').stop().animate({
+                        'width'     :'52px',
+                        'height'    :'52px',
+                        'top'       :'0px',
+                        'left'      :'0px',
+                        'opacity'   :'0.1'
+                    },5000,'easeOutBack');
+
+                    $this.find('a:first,h2').removeClass('active');
+                }
+            );
+            });
+        
