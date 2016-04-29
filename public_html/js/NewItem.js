e77efc7b31a8814afe70473368734eb84948b146
Yo1357 <anders.museth@gmail.com>
2016-04-29 12:12:08 -0700

    added login and deleting posts

--- public_html/js/NewItem.js
+++ public_html/js/NewItem.js
@@ -1,23 +1,47 @@
+
 $(function () {
-   var APPLICATION_ID =  "CC3979E6-F655-FD96-FF13-1FAAD8E3DE00",
-       SECRET_KEY = "76B7A6FF-ACD6-07C5-FFF8-BB69AB961B00",
+    
+   var APPLICATION_ID =  "8B1C2399-1443-6254-FFB8-C4C280680100",
+       SECRET_KEY = "6A63F44E-1BE2-8876-FFE6-18BF2EE4BC00",
        VERSION = "v1";
   
-   
+
    Backendless.initApp(APPLICATION_ID,SECRET_KEY, VERSION);
-   var addBlogScript = $("#add-blog-template").html();
+   $(".button-collapse").sideNav();
+   
+   
+   if(Backendless.UserService.isValidLogin()){
+    userLoggedIn(Backendless.LocalCache.get("current-user-id")); 
+   }
+   else{
+      
+    //var user = new Backendless.User();
+   //user.email = "anders.museth@gmail.com"
+   //user.password = "password";
+   //Backendless.UserService.register(user);
+   
+   
+   
+   var loginScript = $("#login-template").html();
+   var loginTemplate = Handlebars.compile(loginScript);
+    $('.main-container').html(loginTemplate);
+   }
+   
+    $(document).on('submit', '.form-signin', function(event){
+      event.preventDefault();
+      var data = $(this).serializeArray(),
+        email = data[0].value,
+        password = data[1].value;
+        
+     Backendless.UserService.login(email, password, true, new Backendless.Async(userLoggedIn, gotError));
+   });
+   $(document).on('click', '.add-blog', function(){
+      var addBlogScript = $("#add-blog-template").html();
       var addBlogTemplate = Handlebars.compile(addBlogScript);
       
       $('.main-container').html(addBlogTemplate);
-   tinymce.init({ selector:'textarea', selector: "textarea",
-    plugins: [
-        "advlist autolink lists link image charmap print preview anchor",
-        "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste"
-    ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image" });
    
-   
+   });
    $(document).on('submit', '.form-add-blog', function(event){
       event.preventDefault();
  
@@ -32,9 +56,9 @@ $(function () {
   }
   else{
           
-      var dataStore = Backendless.Persistence.of(Posts);
+      var dataStore = Backendless.Persistence.of(Tasks);
      
-      var postObject = new Posts({
+      var postObject = new Tasks({
           title: title,
           content: content,
           authorEmail: Backendless.UserService.getCurrentUser().email
@@ -44,6 +68,49 @@ $(function () {
       dataStore.save(postObject);
       
       this.title.value = "";
-      this.content.value = ""; 
+      this.content.value = "";   });
+   $(document).on('click', '.logout', function(){
+       Backendless.UserService.logout(new Backendless.Async(userLoggedOut, gotError));
+       
+      var loginScript = $("#login-template").html();
+      var loginTemplate = Handlebars.compile(loginScript);
+       $('.main-container').html(loginTemplate); 
    });
-   });
+});
+
+
+function Tasks(args){
+    args = args || "";
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
+
+function userLoggedIn(user) {
+    console.log("user successfully logged in");
+    var userData;
+    if(typeof user == "string"){
+        userData = Backendless.Data.of(Backendless.User).findById(user);
+    } else{
+        userData = user;
+    }
+    var welcomeScript = $('#welcome-template').html();
+    var welcomeTemplate = Handlebars.compile(welcomeScript);
+    var welcomeHTML = welcomeTemplate(userData);
+    
+    $('.main-container').html(welcomeHTML);
+}
+function userLoggedOut(){
+    console.log("successfully logged out")
+}
+
+function gotError(error){
+    console.log("Error message - " + error.message);
+    console.log("Error code - " + error.code);
+Materialize.toast('Wrong Password Or Username!', 2000);
+}
+
+function required() {
+   var x = document.getElementById("myText").required;
+   document.getElementById("demo").innerHTML = x;
+}
--- public_html/js/ToDoList.js
+++ public_html/js/ToDoList.js
@@ -1,39 +1,56 @@
 $(function (){
-   var APPLICATION_ID = "8B1C2399-1443-6254-FFB8-C4C280680100",
-       SECRET_KEY = "F014B1D6-7772-4951-FFB2-160FAE296F00",
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+    }
+  );
+  
+    var APPLICATION_ID = "8B1C2399-1443-6254-FFB8-C4C280680100",
+       SECRET_KEY = "6A63F44E-1BE2-8876-FFE6-18BF2EE4BC00",
        VERSION = "v1";
-       
+    $(document).on('click', '.trash', function(event){
+        Backendless.Persistence.of(Tasks).remove(event.target.attributes.data.nodeValue);
+        location.reload();
+    });   
    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
    
-   var dataStore = Backendless.Persistence.of(Tasks);
+   /*var dataStore = Backendless.Persistence.of(Tasks);
    var task = new Tasks({title: "My First Item", content:"delete ASAP", author:"email@email.com"});
    dataStore.save(task);
+   */
+   var postsCollection = Backendless.Persistence.of(Tasks).find();
    
-   //var wrapper = {
-     //  posts: postsCollection.data
-//   };
   
-//    Handlebars.registerHelper('format', function (time) {
-//     return moment(time).format("dddd, MMMM Do YYYY") ;   
-//});
+    console.log(postsCollection);
+   var wrapper = {
+       posts: postsCollection.data
+   };
    
-//   var postsCollection = Backendless.Persistence.of(Posts).find();
-//   var blogTemplate = Handlebars.compile(blogScript);
-//   var blogHTML = blogTemplate(wrapper);
    
-//   $('.main-container').html(blogHTML);
-//     var blogScript = $("#today-template").html();
-//  var blogTemplate = Handlebars.compile(blogScript);
-//   var blogHTML = blogTemplate(wrapper);
    
-//   $('.new').html(blogHTML);
-//});
+Handlebars.registerHelper('format', function (time) {
+      return moment(time).format("dddd, MMMM Do YYYY") ;   
 });
+   
+   var blogScript = $("#blogs-template").html();
+   var blogTemplate = Handlebars.compile(blogScript);
+   var blogHTML = blogTemplate(wrapper);
+   
+   $('.main-container').html(blogHTML);
+      var blogScript = $("#today-template").html();
+   var blogTemplate = Handlebars.compile(blogScript);
+   var blogHTML = blogTemplate(wrapper);
+   
+    
+});
+
 function Tasks(args){
-    args = args || {};
+    args = args || "";
     this.title = args.title || "";
     this.content = args.content || "";
-    this.author = args.author || "";
+    this.authorEmail = args.authorEmail || "";
 }
 
 
